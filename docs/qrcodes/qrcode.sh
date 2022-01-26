71e6c777d3130de8e0bfa9bb8058f717205f182b
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2022-01-26 16:25:45 +0100

    Add QR Codes

---
+++ docs/qrcodes/qrcode.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-a1" > a1.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-a2" > a2.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-a3" > a3.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-a4" > a4.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-a5" > a5.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-b1" > b1.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-b2" > b2.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-b3" > b3.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-b4" > b4.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-b5" > b5.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-c1" > c1.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-c2" > c2.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-c3" > c3.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-c4" > c4.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-c5" > c5.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-d1" > d1.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-d2" > d2.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-d3" > d3.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-d4" > d4.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-d5" > d5.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-e1" > e1.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-e2" > e2.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-e3" > e3.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-e4" > e4.svg
+qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-e5" > e5.svg
+
+
+
+

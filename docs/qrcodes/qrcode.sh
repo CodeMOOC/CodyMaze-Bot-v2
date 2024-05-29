9ff3ac205c29902c4c8f3e4a43a968fdd8bf1487
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2024-05-29 11:39:29 +0200

    Clean up

--- docs/qrcodes/qrcode.sh
+++ docs/qrcodes/qrcode.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-a1" > a1.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-a2" > a2.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-a3" > a3.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-a4" > a4.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-a5" > a5.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-b1" > b1.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-b2" > b2.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-b3" > b3.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-b4" > b4.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-b5" > b5.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-c1" > c1.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-c2" > c2.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-c3" > c3.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-c4" > c4.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-c5" > c5.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-d1" > d1.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-d2" > d2.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-d3" > d3.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-d4" > d4.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-d5" > d5.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-e1" > e1.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-e2" > e2.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-e3" > e3.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-e4" > e4.svg
-qr --factory=svg "http://t.me/codymazebot?start=neoconnessi-e5" > e5.svg
+# Full QR Code generator, requires Python
 
+export EVENT='giocourbino'
 
-
-
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-a1" > a1.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-a2" > a2.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-a3" > a3.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-a4" > a4.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-a5" > a5.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-b1" > b1.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-b2" > b2.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-b3" > b3.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-b4" > b4.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-b5" > b5.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-c1" > c1.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-c2" > c2.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-c3" > c3.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-c4" > c4.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-c5" > c5.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-d1" > d1.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-d2" > d2.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-d3" > d3.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-d4" > d4.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-d5" > d5.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-e1" > e1.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-e2" > e2.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-e3" > e3.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-e4" > e4.svg
+qr --factory=svg "http://t.me/codymazebot?start=$EVENT-e5" > e5.svg
